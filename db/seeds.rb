3ace159362ca11b35c18eedfb4d6c8f2031caec6
pazuzu111 <pazuzublue111@gmail.com>
2018-01-02 15:54:09 -0500

    populated seed file with beer data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#delete all from beer table
+Beer.delete_all
+
+#seed with ready data
+Beer.create(name: "raging bitch", brewery: "flying dog", style: "belgian ipa", image_url:"http://flyingdogbrewery.com/wp-content/uploads/2011/01/RagingBitch_Bottle.png", abv:8.3)
+Beer.create(name: "snake dog", brewery: "flying dog", style: "ipa", image_url:"http://flyingdogbrewery.com/wp-content/uploads/2011/02/Snake_Bottle.png", abv:7.1)
+Beer.create(name: "bloodline", brewery: "flying dog", style: "blood orange ale", image_url:"http://flyingdogbrewery.com/wp-content/uploads/2014/04/Bloodline_bottle.png", abv:7)
+
+#notify--> output to console
+puts "3 beers added!!!"
