2eb83b8b18430db20baf1cdebcac175fef089595
pazuzu111 <pazuzublue111@gmail.com>
2018-01-03 13:14:01 -0500

    added reactrouter dom with yarn , created controls component

---
+++ client/src/Control.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Router, Route, Switch } from 'react-router'
+
+
+const Controls = () => {
+    return (
+        <div>
+            <h2>BEERS!</h2>
+        </div>
+    )
+
+}
